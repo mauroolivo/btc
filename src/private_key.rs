35a61c7c7e161b7e4f5d98ba3f306adbe22dbf3f
Mauro Olivo <olivomauro@gmail.com>
2025-02-15 12:16:32 +0100

    Deterministic k: rfc6979

--- src/private_key.rs
+++ src/private_key.rs
@@ -5,6 +5,7 @@ use rand::Rng;
 use crate::field_element::FieldElement;
 use crate::point::Point;
 use crate::secp256k1::Secp256k1;
+use rfc6979::consts::U32;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct PrivateKey {
@@ -23,7 +24,10 @@ impl PrivateKey {
         }
     }
 
-    fn sign(&self, z: &BigUint, k: &BigUint) -> Signature {
+    fn sign(&self, z: &BigUint) -> Signature {
+
+        let k = self.deterministic_k(&z);
+
         let s256 = Secp256k1::new();
         let n = &s256.n;
 
@@ -33,13 +37,35 @@ impl PrivateKey {
 
         //let sig = (z + r * &self.secret) * k_inv % n;
         let mut s = (z + &r * &self.secret) * k_inv % n;
-        println!("s before -> {:x}", s);
+
         if s > n / BigUint::from(2u8) {
             s = n - s;
-            println!("s before -> {:x}", s);
         }
-        println!("z -> {:x}", z);
         Signature::new(&r, &s)
+    }
+
+    fn deterministic_k(&self, z: &BigUint) -> BigUint {
+        let s256 = Secp256k1::new();
+        let n_bytes = s256.n.to_bytes_be();
+        let mut n = [0; 32];
+        //let mut p = GenericArray::<u8, 32>::default();
+        n.copy_from_slice(&n_bytes);
+
+        let k_bytes = self.secret.to_bytes_be();
+        let mut k = [0; 32];
+        k.copy_from_slice(&k_bytes);
+
+        let z_bytes = z.to_bytes_be();
+        let mut z = [0; 32];
+        z.copy_from_slice(&z_bytes);
+
+        let h = Sha256::digest(&z);
+
+        // secret, field modulus, hash/digest (modulus reduced), additional data
+        let k = rfc6979::generate_k::<Sha256, U32>(&k.into(), &n.into(), &h, b"");
+            //<Sha256, u32>();
+
+        BigUint::from_bytes_be(&k)
     }
 }
 
@@ -48,54 +74,25 @@ mod tests {
     use num::bigint::Sign;
     use num::BigUint;
     use super::*;
+
     #[test]
     fn sign() {
-        let has256 = Sha256::digest(Sha256::digest(&b"my message"));
-        let z = BigUint::from_bytes_be(has256.as_slice());
+        let s256 = Secp256k1::new();
+        let n = s256.n;
+        let generator = Point::new_secp256k1(&Some(FieldElement::new(&s256.gx, &s256.p)), &Some(FieldElement::new(&s256.gy, &s256.p)));
 
         let has256 = Sha256::digest(Sha256::digest(&b"my secret"));
         let e = BigUint::from_bytes_be(has256.as_slice());
 
         let private_key = PrivateKey::new(&e);
 
-        let k = &BigUint::from(1234567890u32);
+        let has256 = Sha256::digest(Sha256::digest(&b"my message"));
+        let z = BigUint::from_bytes_be(has256.as_slice());
 
-        let s = private_key.sign(&z, &k);
+        let sig = private_key.sign(&z);
 
-        println!("signature -> {}", s);
+        let point = generator * e;
+
+        assert!(point.verify(&z, &sig));
     }
 }
-/*def hex(self):
-return '{:x}'.format(self.secret).zfill(64)
-# end::source13[]
-
-# tag::source14[]
-def sign(self, z):
-k = self.deterministic_k(z)  # <1>
-r = (k * G).x.num
-k_inv = pow(k, N - 2, N)
-s = (z + r * self.secret) * k_inv % N
-if s > N / 2:
-s = N - s
-return Signature(r, s)
-
-def deterministic_k(self, z):
-k = b'\x00' * 32
-v = b'\x01' * 32
-if z > N:
-z -= N
-z_bytes = z.to_bytes(32, 'big')
-secret_bytes = self.secret.to_bytes(32, 'big')
-s256 = hashlib.sha256
-k = hmac.new(k, v + b'\x00' + secret_bytes + z_bytes, s256).digest()
-v = hmac.new(k, v, s256).digest()
-k = hmac.new(k, v + b'\x01' + secret_bytes + z_bytes, s256).digest()
-v = hmac.new(k, v, s256).digest()
-while True:
-v = hmac.new(k, v, s256).digest()
-candidate = int.from_bytes(v, 'big')
-if candidate >= 1 and candidate < N:
-return candidate  # <2>
-k = hmac.new(k, v + b'\x00', s256).digest()
-v = hmac.new(k, v, s256).digest()
-# end::source14[]*/
