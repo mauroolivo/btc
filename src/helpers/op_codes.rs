32bdac870f6037aa2c5c35c00dae9f32e891058a
Mauro Olivo <olivomauro@gmail.com>
2025-03-26 19:42:24 +0100

    test check sig

--- src/helpers/op_codes.rs
+++ src/helpers/op_codes.rs
@@ -1083,10 +1083,21 @@ pub fn op_checksequenceverify(stack: &mut Vec<Vec<u8>>, version: u32, sequence: 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use num::Num;
     #[test]
     fn test_op_hash160() {
         let mut stack: Vec<Vec<u8>> = vec![b"hello world".to_vec()];
         assert_eq!(op_hash160(&mut stack), true);
         assert_eq!(hex::encode(stack[0].clone()), "d7d5ee7824ff93f94c3055af9382c86c68b5ca92");
+    }
+    #[test]
+    fn test_op_checksig() {
+
+        let z = BigUint::from_str_radix("7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d", 16).unwrap();
+        let sec = hex::decode("04887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34").unwrap();
+        let sig = hex::decode("3045022000eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c022100c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab601").unwrap();
+        let mut stack: Vec<Vec<u8>> = vec![sig, sec];
+        assert_eq!(op_checksig(&mut stack, &z), true);
+        assert_eq!(decode_num(stack[0].as_slice()), 1);
     }
 }
