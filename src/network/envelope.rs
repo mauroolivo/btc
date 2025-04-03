6ec01cfe566b24e663662264cba2398d13b914a7
Mauro Olivo <olivomauro@gmail.com>
2025-04-03 18:22:11 +0200

    parse network envelope test

--- src/network/envelope.rs
+++ src/network/envelope.rs
@@ -1,6 +1,9 @@
 use std::fmt;
-use std::fmt::Error;
 use std::io::{Cursor, ErrorKind, Read};
+use std::io::ErrorKind::InvalidData;
+use num::ToPrimitive;
+use crate::helpers::endianness::{little_endian_to_int};
+use crate::helpers::hash256::hash256;
 
 pub const NETWORK_MAGIC: &[u8; 4] = b"\xf9\xbe\xb4\xd9";
 pub const TESTNET_NETWORK_MAGIC: &[u8; 4] = b"\x0b\x11\x09\x07";
@@ -19,21 +22,46 @@ impl NetworkEnvelope {
         };
         NetworkEnvelope { command, payload, magic }
     }
-    pub fn parse(stream: &mut Cursor<Vec<u8>>, testnet: bool) -> Result<Self, Error> {
-        // let mut magic = [0; 4];
-        // stream.read(&mut magic)?;
-        // if magic == b"".as_slice() {
-        //     return Err(Error::new(
-        //         ErrorKind::InvalidData,
-        //         "Connection reset!",
-        //     ));
-        // }
+    pub fn parse(stream: &mut Cursor<Vec<u8>>, testnet: bool) -> Result<Self, std::io::Error> {
+        let mut magic = [0; 4];
+        stream.read(&mut magic)?;
+        if magic == b"".as_slice() {
+            return Err(std::io::Error::new(
+                ErrorKind::InvalidData,
+                "Connection reset!",
+            ));
+        }
+        let expected_magig = match testnet {
+            true => TESTNET_NETWORK_MAGIC,
+            false => NETWORK_MAGIC
+        };
+        if magic.to_vec() != expected_magig.to_vec() {
+            let res = format!("magic is not right {} vs {}", hex::encode(magic), hex::encode(expected_magig));
+            return Err(std::io::Error::new(InvalidData, res));
+        }
+        let mut buffer: [u8;12] = [0; 12];
+        stream.read(&mut buffer)?;
+        let mut command = buffer
+            .into_iter()
+            .rev()
+            .skip_while(|&byte| byte == 0)
+            .collect::<Vec<_>>();
+        command.reverse();
+        let mut buffer = [0; 4];
+        stream.read(&mut buffer)?;
+        let payload_length = little_endian_to_int(&buffer);
 
-        Result::Err(Default::default())
+        let mut checksum = [0; 4];
+        stream.read(&mut checksum)?;
+
+        let payload: Vec<u8> = vec![0; payload_length.to_usize().unwrap()];
+        let hash = hash256(&payload);
+
+        if checksum.as_slice() != (hash[0..4]).iter().as_slice() {
+            return Err(std::io::Error::new(InvalidData, "Checksum mismatch!"));
+        }
+        Ok(NetworkEnvelope::new(command, payload, testnet))
     }
-
-
-
 }
 impl fmt::Display for NetworkEnvelope {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -50,7 +78,7 @@ impl fmt::Display for NetworkEnvelope {
 mod tests {
     use std::io::{Cursor, Read};
     use crate::helpers::endianness::little_endian_to_int;
-    use crate::network::envelope::NETWORK_MAGIC;
+    use crate::network::envelope::{NetworkEnvelope, NETWORK_MAGIC};
     #[test]
     fn test_network_message() {
         let raw_message = hex::decode("f9beb4d976657261636b000000000000650000005df6e0e2").unwrap(); // "verack\0\0\0\0\0\0"
@@ -70,5 +98,25 @@ mod tests {
         println!("{:?}", hex::encode(&magic));
         println!("{:?}", String::from_utf8(command).unwrap());
         println!("{:?}", payload_len);
+    }
+    #[test]
+    fn test_network_envelope_parse() {
+
+        let raw_message = hex::decode("f9beb4d976657261636b000000000000000000005df6e0e2").unwrap();
+        let mut stream = Cursor::new(raw_message);
+        let envelope = NetworkEnvelope::parse(&mut stream, false).unwrap();
+
+        assert_eq!(String::from_utf8(envelope.command).unwrap(), String::from("verack"));
+        assert_eq!(envelope.payload, b"");
+
+        // self.assertEqual(envelope.command, b'verack')
+        // self.assertEqual(envelope.payload, b'')
+        /*
+        msg = bytes.fromhex('f9beb4d976657273696f6e0000000000650000005f1a69d2721101000100000000000000bc8f5e5400000000010000000000000000000000000000000000ffffc61b6409208d010000000000000000000000000000000000ffffcb0071c0208d128035cbc97953f80f2f5361746f7368693a302e392e332fcf05050001')
+        stream = BytesIO(msg)
+        envelope = NetworkEnvelope.parse(stream)
+        self.assertEqual(envelope.command, b'version')
+        self.assertEqual(envelope.payload, msg[24:])
+        */
     }
 }
